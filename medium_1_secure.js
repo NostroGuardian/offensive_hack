d046dbe9206bdeaa00d349fc0a2febf1c6004f0f
NostroGuardian <58729194+NostroGuardian@users.noreply.github.com>
2023-02-06 17:11:50 +0300

    Delete medium_1_secure.js

--- medium_1_secure.js
+++
@@ -1,8 +0,0 @@
-var blackList = [`<script>`, `</script>`, `<SCRIPT>`, `</SCRIPT>`];
-
-document.querySelector('input').onkeyup = function() {
-    var expr = new RegExp(blackList.join('|'));
-    if (this.value.search(expr) !== -1) {
-        this.value = '';
-    }
-}
